0242763909708693c2397d2d0559186b08056c98
Soulbest <owensoulbeast@gmail.com>
2024-08-09 20:48:28 -0700

    test

---
+++ soulbest/monopolySim/Main.java
@@ -0,0 +1,19 @@
+package soulbest.monopolySim;
+
+
+public class Main {
+
+    private static final boolean DEBUG = true;
+
+    public static void main(String[] args) {
+
+        if (DEBUG) {
+
+            System.out.println("----- Testing Imports:   -------");
+            Square.testImport();
+
+        }
+
+    }
+
+}
---
+++ soulbest/monopolySim/Square.java
@@ -0,0 +1,31 @@
+package soulbest.monopolySim;
+
+public class Square {
+
+    // private final boolean DEBUG = false;
+
+    // private final int index;
+    // private final String name;
+
+    // private final int cost;
+    // private final int mortgageValue = cost/2;
+    // private final int[] rent;
+    // private final int houseCost;
+    // private final int hotelCost;
+
+    // private int numHouses;
+
+
+
+    // public Square(int i) {
+    //     index = i;
+    //     cost = 1;
+    // }
+    
+
+    static void testImport() {
+        System.out.println("Square Import Success");
+    }
+
+
+}
